a8ee32252b0201df84198f6cf8e2ec845abf50bc
Manuel Perez Yebra <154430764+ManuelPerezYebra@users.noreply.github.com>
2024-02-29 10:09:18 +0100

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import MainContainer from "./components/main-container/MainContainer";
+import { GlobalStyles } from "./styles/GlobalStyles";
 
 const App = () => {
 	return <>
+	<GlobalStyles/>
 	<MainContainer $display='flex' $bgcolor='orange'>
 	</MainContainer>
 	
