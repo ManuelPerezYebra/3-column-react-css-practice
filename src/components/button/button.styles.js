cfa0716c19cfed1446a7e97ac3aa933f36ffa437
Manuel Perez Yebra <154430764+ManuelPerezYebra@users.noreply.github.com>
2024-02-29 10:03:58 +0100

    Update button.styles.js

--- src/components/button/button.styles.js
+++ src/components/button/button.styles.js
@@ -3,13 +3,20 @@ import styled from 'styled-components';
 const StyledButton = styled.button`
 	color: ${({ $color }) => $color};
 	width: 50%;
-	margin-left: 48px;
+	margin-left: 3rem;
 
-	margin-top: 48px;
-	padding: 15px;
-	border-radius: 20px;
+	margin-top: 3rem;
+	padding: 0.9375rem;
+	border-radius: 1.25rem;
 	border: none;
 	cursor: pointer;
+	@media (hover: hover) {
+		&:hover {
+			color: white;
+			background-color: transparent;
+			border: 1px solid white;
+		}
+	}
 `;
 
 export { StyledButton };
